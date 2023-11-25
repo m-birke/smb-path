976d6d91a308b7f22edf6c8bf0243204e64034f9
m-birke <wesci@web.de>
2023-11-25 11:15:45 +0100

    remove typehinting features for older py versions

--- src/smb_path/smb_path.py
+++ src/smb_path/smb_path.py
@@ -2,19 +2,19 @@ from collections.abc import Generator
 from io import TextIOWrapper
 from os import stat_result
 from pathlib import Path, PosixPath, WindowsPath
-from typing import Self
+from typing import Union
 
 import smbclient
 
 
 class SmbPath:
     def open(  # noqa A003
-        self: Self,
+        self,
         mode: str = "r",
         buffering: int = -1,
-        encoding: str | None = None,
-        errors: str | None = None,
-        newline: str | None = None,
+        encoding: Union[str, None] = None,
+        errors: Union[str, None] = None,
+        newline: Union[str, None] = None,
     ) -> TextIOWrapper:
         return smbclient.open_file(
             str(self), mode=mode, buffering=buffering, encoding=encoding, errors=errors, newline=newline
@@ -23,36 +23,36 @@ class SmbPath:
     def stat(self, *, follow_symlinks: bool = True) -> stat_result:
         return smbclient.stat(str(self), follow_symlinks=follow_symlinks)
 
-    def iterdir(self) -> Generator[Self, None, None]:
+    def iterdir(self) -> Generator:
         dir_list = smbclient.listdir(str(self))
         for el in dir_list:
             yield Path(str(self / el))
 
-    def mkdir(self):
+    def mkdir(self, *args, **kwargs):
         raise NotImplementedError("Function not implemented for SmbPath")
 
-    def rmdir(self):
+    def rmdir(self, *args, **kwargs):
         raise NotImplementedError("Function not implemented for SmbPath")
 
-    def touch(self):
+    def touch(self, *args, **kwargs):
         raise NotImplementedError("Function not implemented for SmbPath")
 
-    def chmod(self):
+    def chmod(self, *args, **kwargs):
         raise NotImplementedError("Function not implemented for SmbPath")
 
-    def unlink(self):
+    def unlink(self, *args, **kwargs):
         raise NotImplementedError("Function not implemented for SmbPath")
 
-    def rename(self):
+    def rename(self, *args, **kwargs):
         raise NotImplementedError("Function not implemented for SmbPath")
 
-    def replace(self):
+    def replace(self, *args, **kwargs):
         raise NotImplementedError("Function not implemented for SmbPath")
 
-    def symlink_to(self):
+    def symlink_to(self, *args, **kwargs):
         raise NotImplementedError("Function not implemented for SmbPath")
 
-    def hardlink_to(self):
+    def hardlink_to(self, *args, **kwargs):
         raise NotImplementedError("Function not implemented for SmbPath")
 
 

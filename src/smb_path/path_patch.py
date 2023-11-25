8884ed97ae1607f3366b8d64ddb1bd1b71563330
m-birke <wesci@web.de>
2023-11-25 11:15:46 +0100

    py3.12 support

--- src/smb_path/path_patch.py
+++ src/smb_path/path_patch.py
@@ -1,5 +1,6 @@
 import os
 import re
+import sys
 from pathlib import Path
 
 import wrapt
@@ -7,29 +8,59 @@ import wrapt
 from smb_path.smb_path import SmbPath, SmbPosixPath, SmbWindowsPath
 
 
-@wrapt.when_imported("pathlib")
-def patch_path_constructor(pathlib):
-    def wrapt_new(original_new, _instance, args, _kwargs):
-        cls = args[0]
-        user_args = args[1:]
+if sys.version_info >= (3, 12):
 
-        if cls is not Path:
-            return original_new(*args)
+    @wrapt.when_imported("pathlib")
+    def patch_path_constructor(pathlib):
+        def wrapt_new(original_new, _instance, args, _kwargs):
+            cls = args[0]
+            user_args = args[1:]
 
-        if not user_args:
-            return original_new(*args)
+            if cls is not Path:
+                return original_new(*args)
 
-        if isinstance(user_args[0], str):
-            # check for server URL pattern of first arg
-            if not re.match(r"(//|\\\\)([a-z0-9]+)((\.[a-z0-9]+)*)(\.[a-z]+){1}", user_args[0]):
-                return original_new(*args)  # not smb
+            if not user_args:
+                return original_new(*args)
 
-        elif not isinstance(user_args[0], SmbPath):
-            return original_new(*args)
+            if isinstance(user_args[0], str):
+                # check for server URL pattern of first arg
+                if not re.match(r"(//|\\\\)([a-z0-9]+)((\.[a-z0-9]+)*)(\.[a-z]+){1}", user_args[0]):
+                    return original_new(*args)  # not smb
 
-        cls = SmbWindowsPath if os.name == "nt" else SmbPosixPath
+            elif not isinstance(user_args[0], SmbPath):
+                return original_new(*args)
 
-        self = cls._from_parts(user_args)
-        return self
+            cls = SmbWindowsPath if os.name == "nt" else SmbPosixPath
 
-    wrapt.wrap_function_wrapper(pathlib.Path, "__new__", wrapt_new)
+            return object.__new__(cls)
+
+        wrapt.wrap_function_wrapper(pathlib.Path, "__new__", wrapt_new)
+
+else:
+
+    @wrapt.when_imported("pathlib")
+    def patch_path_constructor(pathlib):
+        def wrapt_new(original_new, _instance, args, _kwargs):
+            cls = args[0]
+            user_args = args[1:]
+
+            if cls is not Path:
+                return original_new(*args)
+
+            if not user_args:
+                return original_new(*args)
+
+            if isinstance(user_args[0], str):
+                # check for server URL pattern of first arg
+                if not re.match(r"(//|\\\\)([a-z0-9]+)((\.[a-z0-9]+)*)(\.[a-z]+){1}", user_args[0]):
+                    return original_new(*args)  # not smb
+
+            elif not isinstance(user_args[0], SmbPath):
+                return original_new(*args)
+
+            cls = SmbWindowsPath if os.name == "nt" else SmbPosixPath
+
+            self = cls._from_parts(user_args)
+            return self
+
+        wrapt.wrap_function_wrapper(pathlib.Path, "__new__", wrapt_new)
